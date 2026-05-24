02f1e874e38e447e8a3000194ebff828256c5976
birbalkr <birbalkr1435@gmail.com>
2026-05-24 15:12:13 +0530

    delete  all file and make mobile folder

--- constants/themes.ts
+++
@@ -1,9 +0,0 @@
-export const  COLORS = {
-    primary: "#4ADE80",
-    secondary: "#2DD4BF",
-    background: "#000000",
-    surface: "#1A1A1A",
-    surfaceLight: "#2A2A2A",
-    while: "#FFFFFF",
-    gray: "#6B7280",
-}as const;
