cf58365b067b022a26f4ba238ec559bf516cf3fb
birbalkr <birbalkr1435@gmail.com>
2026-05-29 22:50:12 +0530

    Auto Navigation & tabs setup

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,41 @@
+
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons';
+import COLORS from '@/constants/color';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+export default function TabLayout() {
+    const inset = useSafeAreaInsets();
+    return (
+        <Tabs
+        screenOptions={{
+            headerShown: false,
+            headerShadowVisible: false,
+            tabBarActiveTintColor: COLORS.primary,
+            tabBarStyle: {
+                backgroundColor: COLORS.cardBackground,
+                borderTopWidth: 1,
+                borderTopColor: COLORS.border,
+                paddingTop: 5,
+                paddingBottom: inset.bottom,
+                height: 60+inset.bottom,
+            },
+            }}>
+            <Tabs.Screen name="index" 
+            options={{ 
+                title: "Home",
+                tabBarIcon:(({color,size})=>(<Ionicons name='home-outline' size={size} color={color}/>))
+                }} />
+            <Tabs.Screen name="create" 
+            options={{ 
+                title: "Create",
+                tabBarIcon:(({color,size})=>(<Ionicons name='add-circle-outline' size={size} color={color}/>))
+                }} />
+            <Tabs.Screen name="profile" 
+            options={{ 
+                title: "Profile",
+                tabBarIcon:(({color,size})=>(<Ionicons name='person-outline' size={size} color={color}/>))
+                }} />
+        </Tabs>
+    )
+}
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,18 +1,50 @@
-import { Stack } from "expo-router";
-import React from "react";
+import { Stack, Redirect, useSegments } from "expo-router";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
+import { useAuthStore } from "@/store/authStore";
 
 export default function RootLayout() {
+  const segments = useSegments();
+
+  const {
+    checkAuth,
+    user,
+    token,
+    isCheckingAuth,
+  } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  // Wait until auth state is restored
+  if (isCheckingAuth) {
+    return null;
+  }
+
+  const inAuthGroup = segments[0] === "(auth)";
+  const isSignedIn = !!user && !!token;
+
+  // Redirect logic
+  if (!isSignedIn && !inAuthGroup) {
+    return <Redirect href="/(auth)" />;
+  }
+
+  if (isSignedIn && inAuthGroup) {
+    return <Redirect href="/(tabs)" />;
+  }
+
   return (
     <SafeAreaProvider>
       <SafeScreen>
-        <Stack screenOptions={{ headerShown: false }} >
-          <Stack.Screen name="index" />
+        <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(tabs)" />
         </Stack>
       </SafeScreen>
+
       <StatusBar style="dark" />
     </SafeAreaProvider>
   );
